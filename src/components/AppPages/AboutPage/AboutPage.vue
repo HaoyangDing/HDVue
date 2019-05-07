3700580da724219e7eb8cd9dc87e70a6f0e2500e
Haoyang Ding <haoyang_ding@hotmail.com>
2019-05-06 21:09:15 -0400

    pls

--- src/components/AppPages/AboutPage/AboutPage.vue
+++ src/components/AppPages/AboutPage/AboutPage.vue
@@ -1,5 +1,48 @@
 <template>
-  <h1>This will be the ABOUT page??</h1>
+  <v-container grid-list-md justify-space-around >
+    <v-layout row wrap>
+      <v-flex sm6 xs10 offset-sm3 offset-xs1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="3"
+          ></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex sm4 xs10 offset-sm1 offset-xs1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2"
+          ></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex sm2 d-none d-xs-block>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="1"
+          ></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex sm4 xs10 offset-sm2 offset-xs1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2"
+          ></v-img>
+        </v-card>
+      </v-flex>
+      <v-flex sm6 xs10 offset-sm3 offset-xs1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="3"
+          ></v-img>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 
